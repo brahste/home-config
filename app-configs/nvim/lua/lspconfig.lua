174be4b7cc88824d19e90390ece44d5ffa930821
Braden Stefanuk <49038751+brahste@users.noreply.github.com>
2023-09-12 10:10:45 -0700

    feat: add json lsp to nvim

--- app-configs/nvim/lua/lspconfig.lua
+++ app-configs/nvim/lua/lspconfig.lua
@@ -23,6 +23,7 @@ nvim_lsp.bashls.setup{on_attach=on_attach, capabilities=capabilities}
 nvim_lsp.tsserver.setup{on_attach=on_attach, capabilities=capabilities}
 nvim_lsp.html.setup{on_attach=on_attach, capabilities=capabilities}
 nvim_lsp.cssls.setup{on_attach=on_attach, capabilities=capabilities}
+nvim_lsp.jsonls.setup{on_attach=require('lsp-format').on_attach, capabilities=capabilities}
 
 -- Enable Nix lsp
 nvim_lsp.nil_ls.setup{on_attach=on_attach, capabilities=capabilities}
